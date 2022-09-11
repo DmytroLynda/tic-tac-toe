8da86e35c386b6d61d753b5d39934921851a3aaf
adnylortym <adnylortym@gmail.com>
2022-09-11 14:18:48 +0200

    Refactoring & few style fixes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Game } from './Game';
+import { Game } from './components/game';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
