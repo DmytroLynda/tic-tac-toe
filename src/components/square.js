8252b73888562d41495c19631e02d6b2047c520f
adnylortym <adnylortym@gmail.com>
2022-09-11 13:28:01 +0200

    Split folder structure.

---
+++ src/components/square.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
+  render() {
+    return (
+      <button className="square" onClick={() => this.setState({ value: this.state.value ? null : 'X' })}>
+        {this.state.value}
+      </button>
+    );
+  }
+}
