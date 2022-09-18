d03044f09edf369b75a67dc1a2470fb211d4a39c
adnylortym <adnylortym@gmail.com>
2022-09-18 13:58:40 +0200

    Add calculation winner for showing status

--- src/components/board.js
+++ src/components/board.js
@@ -32,8 +32,34 @@ export class Board extends React.Component {
       });
   }
 
+  calculateWinner() {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (this.state.squares[a] && this.state.squares[a] === this.state.squares[b] && this.state.squares[a] === this.state.squares[c]) {
+        return this.state.squares[a];
+      }
+    }
+    return null;
+  }
+
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = this.calculateWinner();
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
