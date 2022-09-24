e0ac67cd8802fb8bea54158143c8058ebaa17fbc
adnylortym <adnylortym@gmail.com>
2022-09-24 13:57:28 +0200

    Add game history. Move state to the game component

--- src/components/board.js
+++ src/components/board.js
@@ -2,73 +2,18 @@ import React from 'react';
 import { Square } from './square';
 
 export class Board extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true
-    }
-  }
-
   renderSquare(squareNumber) {
     return (
       <Square
-        value={this.state.squares[squareNumber]}
-        onClick={() => this.handleClick(squareNumber)}
+        value={this.props.squares[squareNumber]}
+        onClick={() => this.props.onClick(squareNumber)}
       />
     );
   }
 
-  handleClick(squareNumber) {
-    this.handleGameSteep(squareNumber);
-  }
-
-  handleGameSteep(squareNumber) {
-    const squares = this.state.squares.slice();
-
-    if (this.calculateWinner() || squares[squareNumber]){
-      return;
-    }
-
-    squares[squareNumber] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-        squares: squares,
-        xIsNext: !this.state.xIsNext
-      });
-  }
-
-  calculateWinner() {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (this.state.squares[a] && this.state.squares[a] === this.state.squares[b] && this.state.squares[a] === this.state.squares[c]) {
-        return this.state.squares[a];
-      }
-    }
-    return null;
-  }
-
   render() {
-    const winner = this.calculateWinner();
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
         </div>
